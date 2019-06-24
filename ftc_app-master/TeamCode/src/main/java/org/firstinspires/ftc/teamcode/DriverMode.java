9d366f04bb330492eee03075e2783af845bbf132
mirunic15 <miruru2003@gmail.com>
2019-06-24 11:42:44 +0300

    nume orice

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMode.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMode.java
@@ -31,7 +31,7 @@ public class DriverMode extends RobotHardware {
             BL.setPower(Range.clip(gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x, -0.7, 0.7));
             BR.setPower(Range.clip(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x, -0.7, 0.7));
         } else {
-            StopMotors();
+            StopMotors(); // >)
         }
 
     }
